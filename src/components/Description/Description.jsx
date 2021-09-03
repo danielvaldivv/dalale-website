89118b4a922f3beb419de947e7df0bcbe46be166
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-03 17:23:29 -0500

    [Add] Description and picture of products

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import productoImg from '../../assets/picture/productos2.jpg'
+
+import './Description.scss'
+
 function Description() {
   return (
-    <h2>
-      Bienvenido
-    </h2>
+    <section className="Description">
+      <div>
+        <h3>En Dalalé queremos lo mejor para tí y tu famila.</h3>
+        <h4>Es por esto que lanzamos los siguientes productos.</h4>
+      </div>
+      <figure>
+        <img src={productoImg} alt="Productos Dalalé" />
+      </figure>
+    </section>
   )
 }
 
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Description from '../../components/Description/Description'
+import './Home.scss'
 
 function Home() {
   return (
-    <>
+    <main>
       <Description/>
-    </>
+    </main>
   )
 }
 
