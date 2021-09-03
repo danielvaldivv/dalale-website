dd157082c52eb6fc2d09fdeaf54ed3450a0ded86
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-03 18:21:55 -0500

    [Change] Style details of footer and products tittle

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -7,8 +7,8 @@ function Product() {
   return (
     <div className="Product">
       <img src={productImg1} alt="Product" width="240px" />
-      <h1><span>10.000</span></h1>
       <h4>Pasta de ajo</h4>
+      <h1><span>$ 10.000</span></h1>
       <p>Pasta de ajos con especias maravillosas que traen a tu mente los banquetes preparados en la valhalla</p>
     </div>
   )
