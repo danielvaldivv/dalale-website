55c9c789ff5ac19ae7a39d78d5934c6360a35aa8
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-03 18:14:23 -0500

    [Add] Footer structure, styles and icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import istagram from '../../assets/icons/instagram.png'
+import whatsapp from '../../assets/icons/whatsapp.png'
+import './Frooter.scss'
+
 function Footer() {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <footer className="index-footer">
+      <p>Contactanos:</p>
+      <ul className="index-footer-contact">
+          <li><a target="_blank" rel="noreferrer" href="https://www.google.com/"><span><img src={istagram} alt="Logo istagram"/></span></a></li>
+          <li><a target="_blank" rel="noreferrer" href="https://www.google.com/"><span><img src={whatsapp} alt="Logo whatsapp"/></span></a></li>
+      </ul>
+      <p>Copyright © by DALALÉ</p>
+      <p> 2021</p>
+    </footer>
   )
 }
 
