3b2c3015fd8d7bedf916505265dec6854ddecc52
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-04 10:44:46 -0500

    [Add] Initial State and iterator of products

---
+++ src/initialState.js
@@ -0,0 +1,28 @@
+export default {
+  cart: [],
+  buyer: [],
+  orders: [],
+  products: [
+    {
+      id: '1',
+      image: 'https://arepa.s3.amazonaws.com/camiseta.png',
+      title: 'Pasta de Ajo',
+      price: 10000,
+      description: 'Pasta de ajos con especias maravillosas que traen a tu mente los banquetes preparados en la valhalla',
+    },
+    {
+      id: '2',
+      image: 'https://arepa.s3.amazonaws.com/mug.png',
+      title: 'Ghee',
+      price: 12000,
+      description: 'Delicioso Ghee que lleva a tu paladar a caminar de la mano con Anthony Bourdain.',
+    },
+    {
+      id: '3',
+      image: 'https://arepa.s3.amazonaws.com/pin.png',
+      title: 'Humus de Garbanzo',
+      price: 10000,
+      description: 'Maravillozo Humus de Garbanzos que te sientan a compartir con el mismisimo buda.',
+    },
+  ],
+};
