7d8b095287f61c5dd553071e1f2aa51753706bc5
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-06 07:12:39 -0500

    [Add] Product card rotate and header positon:fixed to absolute

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Product.scss'
 
-const Product = ({ product, handleAddToCart }) => (
-    <section className="Product">
-      <div className="Product-content">
-        <img src={product.image} alt="Product" width="240px" />
-        <div className="Product__description">
-          <h4>{product.title}</h4>
-          <h1><span>$ {product.price}</span></h1>
-          <p>{product.description}</p>
-        </div>
+const Product = ({ product, handleAddToCart }) => {
+  const [isRotate, setIsRotate] = useState(false)
+
+  const onRotate = () =>{
+    setIsRotate(rotated => !rotated)
+  }
+    return (
+      <div className="Product">
+        <section className={`Product-card ${isRotate ?'rotated' :''}`}>
+          <div className="Product-front">
+            <div className="Product-content">
+              <img src={product.image} alt="Product" width="240px" />
+              <div className="Product__description">
+                <h4>{product.title}</h4>
+                <h1><span>$ {product.price}</span></h1>
+                <p>{product.description}</p>
+              </div>
+            </div>
+            <p className="read-more" onClick={onRotate}  >Leer más...</p>
+            <button type="button" onClick={handleAddToCart(product)}>
+              Añadir
+            </button>
+          </div>
+          <div className="Product-back">
+            <div className="Product-content">
+              <div className="Product__description">
+                <h4>{product.title}</h4>
+                <p>{product.description}</p>
+              </div>
+            </div>
+            <p className="read-more" onClick={onRotate} >Leer menos...</p>
+          </div>
+        </section>
       </div>
-      <button type="button" onClick={handleAddToCart(product)}>
-        Añadir
-      </button>
-    </section>
-)
+    )
+  }
 
 export default Product
