b90ec08413238802b460d4816f0e3c8cb8496ad6
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-06 11:07:19 -0500

    [Change] footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import dalaleBlack from'../../assets/logo/LOGOnegro.png'
 import istagram from '../../assets/icons/instagram.png'
 import whatsapp from '../../assets/icons/whatsapp.png'
 import './Frooter.scss'
@@ -7,13 +8,15 @@ import './Frooter.scss'
 function Footer() {
   return (
     <footer className="index-footer">
-      <p>Contactanos:</p>
+     <section className="index-footer-container">
       <ul className="index-footer-contact">
-          <li><a target="_blank" rel="noreferrer" href="https://www.google.com/"><span><img src={istagram} alt="Logo istagram"/></span></a></li>
-          <li><a target="_blank" rel="noreferrer" href="https://www.google.com/"><span><img src={whatsapp} alt="Logo whatsapp"/></span></a></li>
-      </ul>
-      <p>Copyright © by DALALÉ</p>
-      <p> 2021</p>
+            <p>Contáctanos:</p>
+            <li><a target="_blank" rel="noreferrer" href="https://www.google.com/"><span><img src={istagram} alt="Logo istagram"/></span></a></li>
+            <li><a target="_blank" rel="noreferrer" href="https://www.google.com/"><span><img src={whatsapp} alt="Logo whatsapp"/></span></a></li>
+        </ul>
+        <img src={dalaleBlack} alt="Logo Dalale" width="100px  "/>
+        <p> Copyright © 2021 Dalalé All rights reserved.</p>
+     </section>
     </footer>
   )
 }
