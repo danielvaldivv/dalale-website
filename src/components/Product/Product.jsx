f5fdc998ec22e27247508cbffd1041e9b908b3eb
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-06 18:46:34 -0500

    [Add] Intersection Observer and @keyframes

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,36 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 import './Product.scss'
 
 const Product = ({ product, handleAddToCart }) => {
-  const [isRotate, setIsRotate] = useState(false)
+  const [isVisible, setIsVisible] = useState(false)
 
-  const onRotate = () =>{
-    setIsRotate(rotated => !rotated)
+  const element = useRef(null)
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver( entries => {
+      const {isIntersecting} = entries[0]
+      if(isIntersecting){
+        setIsVisible(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(element.current)
+  }, [element])
+
+  const onVisible = () =>{
+    setIsVisible(visible => !visible)
   }
-    return (
-      <div className="Product">
-        <h2>{product.title}</h2>
-        <img src={product.image} alt="Product" width="240px" />
-        <section className={`Product-card ${isRotate ?'rotated' :''}`}>
-          <div className="Product-front">
-            <div className="Product-content">
-              <div className="Product__description">
-                <h1><span>$ {product.price}</span></h1>
-                <p>{product.description}</p>
-              </div>
-            </div>
-            <p className="read" onClick={onRotate}  >Leer más...</p>
 
-          </div>
-          <div className="Product-back">
-            <div className="Product-content">
-              <div className="Product__description">
-                <p>{product.description}</p>
+
+    return (
+      <div className="Product" >
+        <h2>{product.title}</h2>
+        <section className="Product-card" ref={element}>
+        {isVisible
+          ? <div className="Product-front">
+            <img src={product.image} alt="Product" width="240px" />
+              <div className="Product-content">
+                <div className="Product__description">
+
+                  <h1><span>$ {product.price}</span></h1>
+                  <p>{product.description}</p>
+                </div>
               </div>
+              <p className="read" onClick={onVisible}  >Leer más...</p>
             </div>
-            <p className="read" onClick={onRotate} >Regresar</p>
-          </div>
+            
+          : <div className="Product-back">
+              <div className="Product-content">
+                <div className="Product__description">
+                  <p>{product.description}</p>
+                </div>
+              </div>
+              <p className="read" onClick={onVisible} >Regresar</p>
+            </div>}
+          
         </section>
         <button className="button-add" type="button" onClick={handleAddToCart(product)}>
             Añadir
