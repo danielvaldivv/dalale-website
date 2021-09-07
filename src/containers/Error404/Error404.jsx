edbba0b06be1747dc57595e73575c415459ed73a
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-07 18:09:23 -0500

    [Add] Error 404 not found component

--- src/containers/Error404/Error404.jsx
+++ src/containers/Error404/Error404.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import './Error404.scss'
+
 function Error404() {
   return (
-    <>
-      <h1>Error</h1>
-    </>
+    <section className="Error404">
+      <h1>Error 404</h1>
+      <h3>No logramos encontrar lo que buscabas :(</h3>
+    </section>
   )
 }
 
