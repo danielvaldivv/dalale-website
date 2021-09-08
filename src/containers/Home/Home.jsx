fd4f7c0bc98a2f03ee7bace7f21701c51f56a8fd
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-07 19:20:24 -0500

    [Add] react-helmet

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from'react-helmet'
 
 import Products from '../../components/Products/Products'
 import Description from '../../components/Description/Description'
@@ -7,6 +8,18 @@ import './Home.scss'
 
 const Home= () => (
   <main>
+    <Helmet>
+      <title>Dalale Gastronomía</title>
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta name="twitter:site" content="@danielvaldivv"/>
+      <meta name="twitter:creator" content="@danielvaldivv"/>
+      <meta name="twitter:title" content="Dalelé Gastronomía"/>
+      <meta name="twitter:description" content="Dalalé Gastronomía"/>
+      <meta
+        name="twitter:image"
+        content="https://i.ibb.co/9sFJw8P/LOGO.png"
+      />
+    </Helmet>
     <Description/>
     <Products />
   </main>
