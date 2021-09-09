68c89d88de8e086da388567c94047b5ca8078d88
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-09-08 21:27:59 -0500

    [Add] pictures and structure description component

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
-import productoImg from '../../assets/picture/family-eating.jpg'
+import familyCooking from '../../assets/picture/family-cooking.jpg'
+import spices from '../../assets/picture/spices.jpg'
 
 import './Description.scss'
 
 function Description() {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const element = useRef(null)
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver( entries => {
+      const {isIntersecting} = entries[0]
+      if(isIntersecting){
+        setIsVisible(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(element.current)
+  }, [element])
+
   return (
     <section className="Description">
       <div className="Description-principal">
         <h3>En Dalalé queremos lo mejor para tí y tú famila.</h3>
         <h4>Es por esto que te ofrecemos los mejores productos para que puedas cocinar las más deliciosas recetas.</h4>
       </div>
-      <figure>
-        <img src={productoImg} alt="Productos Dalalé" />
+      <figure className="figure-family">
+          <img src={familyCooking} alt="Familia Cocinando" />
       </figure>
-      <div className="Description-secondary">
+      <div className="Description-secondary" ref={element}>
+        <figure className="figure-spices">
+          {isVisible &&
+            <img  src={spices} alt="Especias"/>
+          }
+          </figure>
         <h3>La alimentación saludable no tiene porqué que ser aburrida.</h3>
       </div>
     </section>
